2508e9890ae330b3f72443723d0cc00ba76905ca
Melcn <melaniearinosy@outlook.com>
2023-07-12 15:34:46 +0200

    add annotation

--- src/main/java/com/roadtocda/demo/service/BusinessService.java
+++ src/main/java/com/roadtocda/demo/service/BusinessService.java
@@ -1,12 +1,12 @@
 package com.roadtocda.demo.service;
-
+import org.springframework.stereotype.Component;
 import com.roadtocda.demo.model.HelloWorld;
 
+@Component
 public class BusinessService {
 
 	public HelloWorld getHelloWorld() {
-		HelloWorld hw = new HelloWorld();
-		
+		HelloWorld hw = new HelloWorld();	
 		return hw;
 	}
 }
